bb0df72b1b17aeb57b422c8ef362e3d2858bd40b
Carlos <carloskadu2106@gmail.com>
2025-05-29 11:27:28 -0300

    switchcase

---
+++ codigos switch case/switchcase.js
@@ -0,0 +1,546 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let respostaCorreta = 0;
+let respostaIncorreta = 0;
+
+console.log("Qual o meu nome?");
+console.log("A - kadu");
+console.log("B - pedro");
+console.log("C - carlos");
+console.log("D - henrique");
+let pergunta1 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta1 === "A" || pergunta1.toLowerCase() === "kadu":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual minha idade?");
+console.log("A - 16");
+console.log("B - 15");
+console.log("C - 17");
+console.log("D - 14");
+let pergunta2 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta2 === "A" || pergunta2 === "16":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu sobrenome?");
+console.log("A - pereira");
+console.log("B - henrique");
+console.log("C - souza");
+console.log("D - leite");
+let pergunta3 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta3 === "A" || pergunta3.toLowerCase() === "pereira":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu jogo favorito?");
+console.log("A - red dead");
+console.log("B - god of war");
+console.log("C - hollow knight");
+console.log("D - the last of us");
+let pergunta4 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta4 === "C" || pergunta4.toLowerCase() === "hollow knight":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Quando sai silksong?");
+console.log("A - 2026");
+console.log("B - 2027");
+console.log("C - 2028");
+console.log("D - 2025");
+let pergunta5 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta5 === "D" || pergunta5 === "2025":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu estilo preferido?");
+console.log("A - kpop");
+console.log("B - trap");
+console.log("C - reggae");
+console.log("D - jazz");
+let pergunta6 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta6 === "B" || pergunta6.toLowerCase() === "trap":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu estilo de jogo preferido?");
+console.log("A - história");
+console.log("B - competitivo");
+console.log("C - casual");
+console.log("D - simulador");
+let pergunta7 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta7 === "B" || pergunta7.toLowerCase() === "competitivo":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual corte de cabelo eu faço?");
+console.log("A - jaca");
+console.log("B - moica");
+console.log("C - low fade");
+console.log("D - americano");
+let pergunta8 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta8 === "B" || pergunta8.toLowerCase() === "moica":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual o nome do meu canal?");
+console.log("A - kadu player");
+console.log("B - kadu gameplays");
+console.log("C - kaduvip");
+console.log("D - vip");
+let pergunta9 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta9 === "B" || pergunta9.toLowerCase() === "kadu gameplays":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu objetivo antes dos 18?");
+console.log("A - ser rico");
+console.log("B - ser famoso");
+console.log("C - ser pai");
+console.log("D - ganhar na mega");
+let pergunta10 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta10 === "B" || pergunta10.toLowerCase() === "ser famoso":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("\n--- Resultado Final ---");
+console.log("Respostas Corretas: " + respostaCorreta);
+console.log("Respostas Incorretas: " + respostaIncorreta);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+let respostaCorreta = 0;
+let respostaIncorreta = 0;
+
+console.log("Qual o meu nome?");
+console.log("A - kadu");
+console.log("B - pedro");
+console.log("C - carlos");
+console.log("D - henrique");
+let pergunta1 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta1 === "A" || pergunta1.toLowerCase() === "kadu":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual minha idade?");
+console.log("A - 16");
+console.log("B - 15");
+console.log("C - 17");
+console.log("D - 14");
+let pergunta2 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta2 === "A" || pergunta2 === "16":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu sobrenome?");
+console.log("A - pereira");
+console.log("B - henrique");
+console.log("C - souza");
+console.log("D - leite");
+let pergunta3 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta3 === "A" || pergunta3.toLowerCase() === "pereira":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu jogo favorito?");
+console.log("A - red dead");
+console.log("B - god of war");
+console.log("C - hollow knight");
+console.log("D - the last of us");
+let pergunta4 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta4 === "C" || pergunta4.toLowerCase() === "hollow knight":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Quando sai silksong?");
+console.log("A - 2026");
+console.log("B - 2027");
+console.log("C - 2028");
+console.log("D - 2025");
+let pergunta5 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta5 === "D" || pergunta5 === "2025":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu estilo preferido?");
+console.log("A - kpop");
+console.log("B - trap");
+console.log("C - reggae");
+console.log("D - jazz");
+let pergunta6 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta6 === "B" || pergunta6.toLowerCase() === "trap":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu estilo de jogo preferido?");
+console.log("A - história");
+console.log("B - competitivo");
+console.log("C - casual");
+console.log("D - simulador");
+let pergunta7 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta7 === "B" || pergunta7.toLowerCase() === "competitivo":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual corte de cabelo eu faço?");
+console.log("A - jaca");
+console.log("B - moica");
+console.log("C - low fade");
+console.log("D - americano");
+let pergunta8 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta8 === "B" || pergunta8.toLowerCase() === "moica":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual o nome do meu canal?");
+console.log("A - kadu player");
+console.log("B - kadu gameplays");
+console.log("C - kaduvip");
+console.log("D - vip");
+let pergunta9 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta9 === "B" || pergunta9.toLowerCase() === "kadu gameplays":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("Qual meu objetivo antes dos 18?");
+console.log("A - ser rico");
+console.log("B - ser famoso");
+console.log("C - ser pai");
+console.log("D - ganhar na mega");
+let pergunta10 = prompt("Digite sua resposta: ");
+switch (true) {
+  case pergunta10 === "B" || pergunta10.toLowerCase() === "ser famoso":
+    console.log("Certa resposta");
+    respostaCorreta++;
+    break;
+  default:
+    console.log("Resposta Incorreta");
+    respostaIncorreta++;
+    break;
+}
+
+console.log("\n--- Resultado Final ---");
+console.log("Respostas Corretas: " + respostaCorreta);
+console.log("Respostas Incorretas: " + respostaIncorreta);
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+console.log("Qual sistema operacional você quer usar?\n1 - Terminal Linux\n2 - CMD Windows");
+
+let sistema = Number(prompt("Digite sua escolha: "));
+
+if (sistema === 1) {
+  console.log("\nVocê escolheu Terminal Linux");
+
+  let comandos = prompt("Digite o comando para saber o que ele faz: ");
+  switch (comandos) {
+    case "cd ..":
+      console.log("Quando você executa o 'cd ..', está dizendo ao terminal para voltar um nível na hierarquia de diretórios.");
+      break;
+
+    case "mkdir":
+      console.log("Isso cria um diretório com o nome especificado no local atual.");
+      break;
+
+    case "ls":
+      console.log("Sem argumentos, ele lista os arquivos e pastas no diretório atual.");
+      break;
+
+    case "rm":
+      console.log("Remover um arquivo.");
+      break;
+
+    case "clear":
+      console.log("Ele não apaga nada do sistema, nem fecha o terminal — só limpa a exibição atual.");
+      break;
+
+    case "cp":
+      console.log("Copiar um arquivo para outro lugar (ou com outro nome).");
+      break;
+
+    case "mv":
+      console.log("Serve para mover ou renomear arquivos e diretórios.");
+      break;
+
+    case "cat":
+      console.log("Exibir o conteúdo de arquivos no terminal.");
+      break;
+
+    case "echo":
+      console.log("O 'echo' serve para exibir mensagens no terminal ou gravar texto em arquivos.");
+      break;
+
+    default:
+      console.log("Não temos esse comando.");
+  }
+
+} else if (sistema === 2) {
+  console.log("\nVocê escolheu o CMD Windows");
+
+  let comandos = prompt("Digite o comando para saber o que ele faz: ");
+  switch (comandos) {
+    case "mkdir":
+      console.log("Serve para criar uma nova pasta (ou diretório).");
+      break;
+
+    case "dir":
+      console.log("É usado para listar o conteúdo de um diretório (pastas e arquivos).");
+      break;
+
+    case "cd":
+      console.log("É usado para navegar entre pastas (diretórios).");
+      break;
+
+    case "cd ..":
+      console.log("Ele volta um nível na estrutura de pastas.");
+      break;
+
+    case "del":
+      console.log("É usado para deletar (apagar) arquivos.");
+      break;
+
+    case "cls":
+      console.log("Apaga todo o conteúdo visível da janela do CMD — só limpa a tela.");
+      break;
+
+    case "echo":
+      console.log("Ele exibe mensagens no terminal ou controla a exibição de comandos em scripts.");
+      break;
+
+    case "copy":
+      console.log("É usado para copiar arquivos de um lugar para outro.");
+      break;
+
+    case "move":
+      console.log("É usado para mover arquivos ou pastas, ou até renomear.");
+      break;
+
+    case "rmdir":
+      console.log("Serve para remover pastas (diretórios) no CMD do Windows.");
+      break;
+
+    case "exit":
+      console.log("Ele fecha a janela do terminal (ou encerra o processo do CMD).");
+      break;
+
+    default:
+      console.log("Não temos esse comando.");
+  }
+
+} else {
+  console.log("Não temos esse sistema operacional.");
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+let idade = 27;
+let categoria = "";
+
+if (idade < 12) {
+    categoria = "criança";
+} else if (idade >= 12 && idade < 18) {
+    categoria = "adolescente";
+} else if (idade >= 18 && idade <= 60) {
+    categoria = "adulto";
+} else if (idade > 60 && idade <= 99) {
+    categoria = "idosa";
+}
+
+console.log("Categoria: " + categoria);
+console.log("Idade final: " + idade);
+
+// Esse código mostra a idade junto com a categoria correspondente àquela idade
+
+let idade = 18;
+
+if (idade >= 16 && idade < 18) {
+    console.log("Voto facultativo");
+} else if (idade < 16) {
+    console.log("Voto não obrigatório");
+} else if (idade >= 18 && idade <= 69) {
+    console.log("Voto obrigatório");
+}
+
+
+let numero = -10;
+
+if (numero < 0) {
+    console.log("Número negativo");
+} else if (numero === 0) {
+    console.log("Zero");
+} else {
+    console.log("Número positivo");
+}
+
+
+let combustivel = "Gasolina";
+
+if (combustivel === "Gasolina") {
+    console.log("Você escolheu gasolina");
+} else if (combustivel === "Etanol") {
+    console.log("Você escolheu Etanol");
+} else if (combustivel === "Diesel") {
+    console.log("Você escolheu Diesel");
+} else {
+    console.log("Não temos esse combustível");
+}
+
+
+let velocidade = 81;
+
+if (velocidade <= 60) {
+    console.log("Dentro do limite");
+} else if (velocidade > 60 && velocidade <= 80) {
+    console.log("Atenção");
+} else {
+    console.log("Acima do limite");
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
