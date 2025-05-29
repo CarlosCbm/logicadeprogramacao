c6b356ece71d6d2289f52d648c11c6ff85d39c7f
Carlos <carloskadu2106@gmail.com>
2025-05-29 10:30:22 -0300

    codigosiniciais

---
+++ codigosiniciais.js
@@ -0,0 +1,82 @@
+
+Códigos iniciais
+
+
+
+
+
+
+let nome = ("carlos")
+console.log (nome)
+// Declarando uma variável e exibindo no console
+
+let idade = "16"
+console.log (idade)
+// Declarando um número e exibindo no console
+
+let aprendendoJavaScript = "true"
+console.log (aprendendoJavaScript)
+// Criando uma variável booleana
+
+let sobrenome = ("eduardo")
+let nomeCompleto = nome + sobrenome
+console.log (nomeCompleto)
+// Concatenando strings
+
+let numero1 = 10
+let numero2 = 30
+let soma = numero1 + numero2
+console.log (soma)
+// Somando dois números
+
+let numero3 = 10
+let numero4 = 10
+let multiplicacao = numero3 * numero4
+console.log (multiplicacao)
+// Multiplicando dois números
+
+let cidade = "ferraz de vasconcelos"
+console.log (cidade)
+
+let habitantes = 179.198
+console.log (habitantes)
+
+let preco = 93
+console.log (preco)
+
+preco = 213
+console.log (preco)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+let meuValor
+console.log(meuValor)
+// Criando uma variável sem valor (undefined)
+
+let meuNulo = "null"
+console.log(meuNulo)
+// Criando um valor null
+
+let meuNulo = null
+console.log(meuNulo)
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+let nome = prompt("Digite seu nome:")
+let idade = Number(prompt("Digite a sua idade: "))
+let email = prompt("Digite seu email: ")
+let telefone = Number(prompt("Digite seu número de telefone: "))
+console.log("O seu nome é:", nome, "você tem", idade, "anos. O seu email é:", email, "e o seu número de telefone é:", telefone, "correto? Seja bem vindo ao nosso site!")
+
+let anoNascimento = Number(prompt("Digite seu ano de nascimento: "))
+let subtração = 2025 - anoNascimento
+console.log("A sua idade é:", subtração)
+
+let apelido = prompt("Digite seu apelido:")
+console.log("O seu apelido é:", apelido, "belo apelido!")
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
